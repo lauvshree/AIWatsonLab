dda383fec1359b465078b976031b0e29f127bf6c
Lavanya Thiruvali Sunderarajan <lavanyas@skillup.tech>
2021-06-16 15:05:42 +0530

    Added new mp3

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from ibm_watson import SpeechToTextV1
 from ibm_watson import AssistantV2
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, render_template, request
+import sys
 
 load_dotenv(find_dotenv())
 
@@ -26,11 +27,11 @@ def file_uploader():
    return render_template('upload.html')
 
 
-def speechToText(filename):
+def speechToText(filename, extn):
     recognition_service=SpeechToTextV1(IAMAuthenticator(stt_api))
     recognition_service.set_service_url(stt_url)
-    SPEECH_EXTENSION="*.mp3"
-    SPEECH_AUDIOTYPE="audio/mp3"
+    SPEECH_EXTENSION="*."+extn
+    SPEECH_AUDIOTYPE="audio/"+extn
     audio_file=open(filename,"rb")
     result=recognition_service.recognize(audio=audio_file, content_type=SPEECH_AUDIOTYPE).get_result()
     return result["results"][0]["alternatives"][0]["transcript"]
@@ -43,17 +44,27 @@ def getResponseFromAssistant(chat_text):
     print(session_id)
     response=assistant.message(assistant_id=ASSISTANT_ID,session_id=session_id, 
 input={'message_type': 'text','text': chat_text}).get_result()
+    print(response)
     return response["output"]["generic"][0]["text"]
 
 @app.route('/uploader', methods = ['POST'])
 def upload_file():
    if request.method == 'POST':
         f = request.files['file']
-        if f.filename != '':
-            f.save(f.filename)
-            speechToText(f.filename)
-            stt_text=speechToText(f.filename)
-            return getResponseFromAssistant(stt_text)
-		
+        try:
+            if f.filename != '':
+                l = len(f.filename)
+                extn = f.filename[l-3:l]
+                if extn not in ["mp3","wav"]:
+                    raise Exception("Sorry, the file type is unsupported. Try .mp3 or .wav files")
+                print("The file type is ",extn )
+                f.save(f.filename)
+                stt_text=speechToText(f.filename,extn)
+                return getResponseFromAssistant(stt_text)
+            else:
+                raise Exception("Sorry. No filename recognized")
+        except Exception as excp:
+            print(excp)
+            return str(excp),500
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
